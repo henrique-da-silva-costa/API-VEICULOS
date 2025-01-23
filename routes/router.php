e69b465040f640a19140832091b68354e21c5df3
Henrique <81424965+henrique-da-silva-costa@users.noreply.github.com>
2025-01-23 16:57:15 -0300

    ok

--- routes/router.php
+++ routes/router.php
@@ -22,48 +22,32 @@ function carregar(string $controller, string $acao)
 }
 
 $routes = [
-    //Carro
     "GET" => [
         "/carro" => fn() => carregar("Carro", "todos"),
-        "/carro/unico" => fn() => carregar("Carro", "pegarPorId")
+        "/carro/unico" => fn() => carregar("Carro", "pegarPorId"),
+
+        "/caminhao" => fn() => carregar("Caminhao", "todos"),
+        "/caminhao/unico" => fn() => carregar("Caminhao", "pegarPorId"),
+
+        "/moto" => fn() => carregar("Moto", "todos"),
+        "/moto/unico" => fn() => carregar("Moto", "pegarPorId"),
     ],
 
     "POST" => [
         "/carro/cadastrar" => fn() => carregar("Carro", "cadastrar"),
-        "/carro/editar" => fn() => carregar("Carro", "editar")
-    ],
+        "/carro/editar" => fn() => carregar("Carro", "editar"),
 
-    "DELETE" => [
-        "/carro/excluir" => fn() => carregar("Carro", "excluir"),
-    ],
-
-    //MOTO
-    "GET" => [
-        "/moto" => fn() => carregar("Moto", "todos"),
-        "/moto/unico" => fn() => carregar("Moto", "pegarPorId")
-    ],
-
-    "POST" => [
         "/moto/cadastrar" => fn() => carregar("Moto", "cadastrar"),
-        "/moto/editar" => fn() => carregar("Moto", "editar")
-    ],
+        "/moto/editar" => fn() => carregar("Moto", "editar"),
 
-    "DELETE" => [
-        "/moto/excluir" => fn() => carregar("Moto", "excluir"),
-    ],
-
-    //CAMINHÃO
-    "GET" => [
-        "/caminhao" => fn() => carregar("Caminhao", "todos"),
-        "/caminhao/unico" => fn() => carregar("Caminhao", "pegarPorId")
-    ],
-
-    "POST" => [
         "/caminhao/cadastrar" => fn() => carregar("Caminhao", "cadastrar"),
         "/caminhao/editar" => fn() => carregar("Caminhao", "editar")
     ],
 
+
     "DELETE" => [
-        "/caminhao/excluir" => fn() => carregar("Caminhao", "excluir"),
+        "/carro/excluir" => fn() => carregar("Carro", "excluir"),
+        "/moto/excluir" => fn() => carregar("Moto", "excluir"),
+        "/caminhao/excluir" => fn() => carregar("Caminhao", "excluir")
     ]
 ];
